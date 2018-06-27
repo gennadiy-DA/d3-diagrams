04967ed7a1f047710670deb7ed446a9c9b8d85e2
grybin <Gennadiy.Rybin@dataart.com>
2018-06-27 17:36:32 +0300

    example2 works!

--- src/app/d3v3-example2/d3v3-example2.component.ts
+++ src/app/d3v3-example2/d3v3-example2.component.ts
@@ -22,42 +22,154 @@ export class D3v3Example2Component implements OnInit {
 
 
   ngOnInit() {
-    this.width = 1200 - this.margin.left - this.margin.right;
-    this.height = 680 - this.margin.top - this.margin.bottom;
-    this.formatNumber = d3.format(',.0f');    // zero decimal places
-    this.format = d => this.formatNumber(d) + ' ' + this.units;
-    this.color = d3.scale.category20();
+//     this.width = 1200 - this.margin.left - this.margin.right;
+//     this.height = 680 - this.margin.top - this.margin.bottom;
+//     this.formatNumber = d3.format(',.0f');    // zero decimal places
+//     this.format = d => this.formatNumber(d) + ' ' + this.units;
+//     this.color = d3.scale.category20();
+//
+//     // append the svg canvas to the page
+//     this.svg = d3.select('#chart').append('svg')
+//       .attr('width', this.width + this.margin.left + this.margin.right)
+//       .attr('height', this.height + this.margin.top + this.margin.bottom)
+//       .attr('viewBox', '0 0 1600 680') // needs investigation
+//       .attr('preserveAspectRatio', 'xMinYMin meet') // needs investigation
+//       .append('g')
+//       .attr('transform',
+//         'translate(' + this.margin.left + ',' + this.margin.top + ')');
+//
+//     // Set the sankey diagram properties
+//     this.sankey = d3sankey.sankey()
+//                     .nodeWidth(36)
+//                     .nodePadding(40)
+//                     .size([this.width, this.height]);
+//     this.path = this.sankey.link;
+//
+//
+//     // load the data
+//     d3.json('../assets/json/example2.json', (error, graph) => {
+//       const nodeMap = {};
+//       graph.nodes.forEach(x => nodeMap[x.name] = x);
+//       graph.links = graph.links.map(x => {
+//         return {
+//           source: nodeMap[x.source],
+//           target: nodeMap[x.target],
+//           value: x.value
+//         };
+//       });
+//       const nodeColors = {
+//         'Advertiser1': '#FF0000',
+//         'Agency1': '#0000FF',
+//         'Agency2': '#0000FF',
+//         'Agency3': '#0000FF',
+//         'MediaOwner1': '#00FF00',
+//         'MediaOwner2': '#00FF00',
+//         'MediaOwner3': '#00FF00',
+//         'MediaOwner4': '#00FF00',
+//       };
+//
+//       console.dir(this.sankey);
+//       this.sankey
+//         .nodes(graph.nodes)
+//         .links(graph.links)
+//         .layout(32);
+//
+//       // add in the links
+//       const link = this.svg.append('g').selectAll('.link')
+//         .data(graph.links)
+//         .enter().append('path')
+//         .attr('class', 'link')
+//         .attr('d', this.path)
+//         .style('stroke-width', function(d) { return Math.max(1, d.dy); })
+//         .sort((a, b) => b.dy - a.dy);
+//
+//       // add the link titles
+//       link.append('title')
+//         .text(d => {
+//           return d.source.name + ' → ' +
+//             d.target.name + '\n' + this.format(d.value); });
+//
+//       // add in the nodes
+//       const node = this.svg.append('g').selectAll('.node')
+//         .data(graph.nodes)
+//         .enter().append('g')
+//         .attr('class', 'node')
+//
+//         .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')')
+//         .call(d3.behavior.drag()
+//           .origin(function(d: any) { return d; } )
+//           .on('dragstart', function() { return this.parentNode.appendChild(this); })
+//           .on('drag', dragmove));
+// // add the rectangles for the nodes
+//       node.append('rect')
+//         .attr('height', d => d.dy)
+//         .attr('width', this.sankey.nodeWidth())
+//         .style('fill', d => d.color = this.color(d.name.replace(/ .*/, '')))
+//         .style('stroke', d => d3.rgb(d.color).darker(2))
+//         .append('title')
+//         .text(d => d.name + '\n' + this.format(d.value));
+//
+//       // add in the title for the nodes
+//       node.append('text')
+//         .attr('x', -6)
+//         .attr('y', d => d.dy / 2 )
+//         .attr('dy', '.35em')
+//         .attr('text-anchor', 'end')
+//         .attr('transform', null)
+//         .text(d => d.name)
+//         .filter(d => d.x < this.width / 2)
+//         .attr('x', 6 + this.sankey.nodeWidth())
+//         .attr('text-anchor', 'start');
+//
+//       // the function for moving the nodes
+//       function dragmove(d: any) {
+//         d3.select(this).attr('transform',
+//           'translate(' + d.x + ',' + (
+//             d.y = Math.max(0, Math.min(this.height - d.dy, (<any>d3.event).y))
+//           ) + ')');
+//         this.sankey.relayout();
+//         link.attr('d', this.path);
+//       }
+//
+//     });
 
-    // append the svg canvas to the page
-    this.svg = d3.select('#chart').append('svg')
-      .attr('width', this.width + this.margin.left + this.margin.right)
-      .attr('height', this.height + this.margin.top + this.margin.bottom)
-      .attr('viewBox', '0 0 1600 680') // needs investigation
-      .attr('preserveAspectRatio', 'xMinYMin meet') // needs investigation
+
+    var units = 'Orders';
+    var margin = {top: 10, right: 10, bottom: 10, left: 10},
+      width = 1500 - margin.left - margin.right,
+      height = 680 - margin.top - margin.bottom;
+    var formatNumber = d3.format(',.0f'),    // zero decimal places
+      format = function(d) { return formatNumber(d) + ' ' + units; },
+      color = d3.scale.category20();
+// append the svg canvas to the page
+    var svg = d3.select('#chart').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('viewBox', '0 0 1600 680') //needs investigation
+      .attr('preserveAspectRatio', 'xMinYMin meet') //needs investigation
       .append('g')
       .attr('transform',
-        'translate(' + this.margin.left + ',' + this.margin.top + ')');
+        'translate(' + margin.left + ',' + margin.top + ')');
 
     // Set the sankey diagram properties
-    this.sankey = d3sankey.sankey()
-                    .nodeWidth(36)
-                    .nodePadding(40)
-                    .size([this.width, this.height]);
-    this.path = this.sankey.link;
-
+    let sankey = d3sankey.sankey()
+      .nodeWidth(36)
+      .nodePadding(40)
+      .size([width, height]);
+    let path = sankey.link();
 
     // load the data
-    d3.json('../assets/json/example2.json', (error, graph) => {
-      const nodeMap = {};
-      graph.nodes.forEach(x => nodeMap[x.name] = x);
-      graph.links = graph.links.map(x => {
+    d3.json('../assets/json/example2.json', function(error, graph) {
+      var nodeMap = {};
+      graph.nodes.forEach(function(x) { nodeMap[x.name] = x; });
+      graph.links = graph.links.map(function(x) {
         return {
           source: nodeMap[x.source],
           target: nodeMap[x.target],
           value: x.value
         };
       });
-      const nodeColors = {
+      var nodeColors = {
         'Advertiser1': '#FF0000',
         'Agency1': '#0000FF',
         'Agency2': '#0000FF',
@@ -68,69 +180,67 @@ export class D3v3Example2Component implements OnInit {
         'MediaOwner4': '#00FF00',
       };
 
-      console.dir(this.sankey);
-      this.sankey
+      sankey
         .nodes(graph.nodes)
         .links(graph.links)
         .layout(32);
-
-      // add in the links
-      const link = this.svg.append('g').selectAll('.link')
+// add in the links
+      var link = svg.append('g').selectAll('.link')
         .data(graph.links)
         .enter().append('path')
         .attr('class', 'link')
-        .attr('d', this.path)
-        .style('stroke-width', function(d) { return Math.max(1, d.dy); })
-        .sort((a, b) => b.dy - a.dy);
-
-      // add the link titles
+        .attr('d', path)
+        .style('stroke-width', function(d: any) { return Math.max(1, d.dy); })
+        .sort(function(a: any, b: any) { return b.dy - a.dy; });
+// add the link titles
       link.append('title')
-        .text(d => {
+        .text(function(d: any) {
           return d.source.name + ' → ' +
-            d.target.name + '\n' + this.format(d.value); });
-
-      // add in the nodes
-      const node = this.svg.append('g').selectAll('.node')
+            d.target.name + '\n' + format(d.value); });
+// add in the nodes
+      var node = svg.append('g').selectAll('.node')
         .data(graph.nodes)
         .enter().append('g')
         .attr('class', 'node')
 
-        .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')')
+        .attr('transform', function(d: any) {
+          return 'translate(' + d.x + ',' + d.y + ')'; })
         .call(d3.behavior.drag()
-          .origin(function(d: any) { return d; } )
-          .on('dragstart', function() { return this.parentNode.appendChild(this); })
+          .origin(function(d: any) { return d; })
+          .on('dragstart', function() {
+            this.parentNode.appendChild(this); })
           .on('drag', dragmove));
 // add the rectangles for the nodes
       node.append('rect')
-        .attr('height', d => d.dy)
-        .attr('width', this.sankey.nodeWidth())
-        .style('fill', d => d.color = this.color(d.name.replace(/ .*/, '')))
-        .style('stroke', d => d3.rgb(d.color).darker(2))
+        .attr('height', function(d: any) { return d.dy; })
+        .attr('width', sankey.nodeWidth())
+        .style('fill', function(d: any) {
+          return d.color = color(d.name.replace(/ .*/, '')); })
+        // .style('stroke', function(d: any) {
+        //   return d3.rgb(d.color).darker(2); })
         .append('title')
-        .text(d => d.name + '\n' + this.format(d.value));
-
-      // add in the title for the nodes
+        .text(function(d: any) {
+          return d.name + '\n' + format(d.value); });
+// add in the title for the nodes
       node.append('text')
         .attr('x', -6)
-        .attr('y', d => d.dy / 2 )
+        .attr('y', function(d: any) { return d.dy / 2; })
         .attr('dy', '.35em')
         .attr('text-anchor', 'end')
         .attr('transform', null)
-        .text(d => d.name)
-        .filter(d => d.x < this.width / 2)
-        .attr('x', 6 + this.sankey.nodeWidth())
+        .text(function(d: any) { return d.name; })
+        .filter(function(d: any) { return d.x < width / 2; })
+        .attr('x', 6 + sankey.nodeWidth())
         .attr('text-anchor', 'start');
-
-      // the function for moving the nodes
+// the function for moving the nodes
       function dragmove(d: any) {
         d3.select(this).attr('transform',
           'translate(' + d.x + ',' + (
-            d.y = Math.max(0, Math.min(this.height - d.dy, (<any>d3.event).y))
+            d.y = Math.max(0, Math.min(height - d.dy, (<any>d3.event).y))
           ) + ')');
-        this.sankey.relayout();
-        link.attr('d', this.path);
+        sankey.relayout();
+        link.attr('d', path);
       }
-
     });
   }
 
